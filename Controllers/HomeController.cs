420af49a3f69ae8607940b828c19d4fed4f9ed9f
Khowfsix <89312102+Khowfsix@users.noreply.github.com>
2022-10-09 14:21:44 +0700

    phat

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private sneakerShopEntities1 db = new sneakerShopEntities1();
+
         public ActionResult Index()
         {
-            return View();
+            var product = db.Product.Include(p => p.Category).Include(p => p.Stock);
+            return View(product.ToList());
         }
 
         public ActionResult About()
